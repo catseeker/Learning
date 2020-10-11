e2a6756f9ae0fb9d5a6daa13c507fa9152029047
catseeker <453679872@qq.com>
2020-10-11 23:14:19 +0800

    add a new file

---
+++ CppLession1/.history/test_20201011225150.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2020-10-11 22:50:15
+ * @LastEditTime: 2020-10-11 22:51:50
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \CppLession1\test.cpp
+ */
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello Greenman Cheng"<<endl;
+    return 0;
+}
---
+++ CppLession1/test.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2020-10-11 22:50:15
+ * @LastEditTime: 2020-10-11 22:54:12
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \CppLession1\test.cpp
+ */
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello Greenman Cheng"<<endl;
+    system("pause");
+    return 0;
+}
